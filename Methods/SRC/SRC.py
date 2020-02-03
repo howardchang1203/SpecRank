e08cba3e88d43d69470eff2a3ced60dd78b179dc
howardchang1203 <60567249+howardchang1203@users.noreply.github.com>
2020-02-03 14:16:32 +0800

    Update SRC.py

--- Methods/SRC/SRC.py
+++ Methods/SRC/SRC.py
@@ -1,16 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug  2 15:08:55 2019
 
-@author: Howard
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 30 17:25:38 2019
-
-@author: Howard
-"""
 from sklearn.cluster import KMeans
 import numpy as np
 import pandas as pd
